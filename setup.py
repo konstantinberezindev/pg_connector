385a0fba2d14f7fc27a53441a76c13567a24636f
konstantinberezin <world.wow2018@gmail.com>
2024-02-04 13:58:16 +0400

    добавлена дополнительная информация про библиотеку

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="pg_connector",
+    description='Simple python library to work with asyncpg',
+    author='Konstantin Berezin',
+    author_email='mrkonstantinberezin@gmail.com',
     version="0.1",
     packages=find_packages(),
     install_requires=[
