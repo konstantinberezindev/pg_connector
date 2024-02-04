1eef158e1bdf7750f39f019957a8ac39917277e7
konstantinberezin <world.wow2018@gmail.com>
2024-02-04 15:16:42 +0400

    Изменена версия

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Simple python library to work with asyncpg',
     author='Konstantin Berezin',
     author_email='mrkonstantinberezin@gmail.com',
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     install_requires=[
         "asyncpg==0.29.0",
